c1813aa873ecce62a82fb8b3efb726f0927e62a0
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2023-01-05 15:43:35 +0100

    Ajouts prite zombie

--- Project1/Game1.cs
+++ Project1/Game1.cs
@@ -29,6 +29,8 @@ namespace Project1
         private MonoGame.Extended.Sprites.AnimatedSprite _perso;
         private int vitesse = 100;
 
+        private Vector2 _positionZombie;
+        private MonoGame.Extended.Sprites.AnimatedSprite _zombie;
 
         StartScreen startscreen;
         GameScreen gamescreen;
@@ -65,7 +67,8 @@ namespace Project1
 
             //Joueur
             _positionPerso= new Vector2(_graphics.PreferredBackBufferWidth / 3 , _graphics.PreferredBackBufferHeight / 2);
-            
+            _positionZombie = new Vector2(_graphics.PreferredBackBufferWidth / 3, _graphics.PreferredBackBufferHeight / 2);
+
             base.Initialize();
         }
 
@@ -87,8 +90,11 @@ namespace Project1
             SpriteSheet spriteSheet = Content.Load<SpriteSheet>("animation.sf", new JsonContentLoader());
             _perso = new MonoGame.Extended.Sprites.AnimatedSprite(spriteSheet);
 
+            SpriteSheet spriteSheetZombie = Content.Load<SpriteSheet>("zombieAnim.sf", new JsonContentLoader());
+            _zombie = new MonoGame.Extended.Sprites.AnimatedSprite(spriteSheetZombie);
 
-       
+
+
 
 
 
@@ -169,8 +175,10 @@ namespace Project1
             _tiledMapRenderer.Update(gameTime);
 
             _perso.Update(deltaTime); // time écoulé
-
             
+            _zombie.Play("idle");
+            _zombie.Update(deltaTime);
+
             base.Update(gameTime);
         }
 
@@ -180,6 +188,7 @@ namespace Project1
             _tiledMapRenderer.Draw();
             _spriteBatch.Begin();
             _spriteBatch.Draw(_perso, _positionPerso);
+            _spriteBatch.Draw(_zombie, _positionZombie);
             _spriteBatch.End();
             // TODO: Add your drawing code here
             base.Draw(gameTime);
