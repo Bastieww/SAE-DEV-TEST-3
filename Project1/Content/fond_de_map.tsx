48af9db48fa3679fcdc7fc1c397485eba8297143
Bastien Vincent Dekens <bastien.vincent-dekens@etu.univ-smb.fr>
2023-01-09 22:21:35 +0100

    efqfq

--- Project1/Content/fond_de_map.tsx
+++ Project1/Content/fond_de_map.tsx
@@ -1,12 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="fond_de_map" tilewidth="32" tileheight="32" tilecount="9" columns="3">
- <image source="fond_de_map.png" width="96" height="96"/>
- <tile id="0" probability="0.11"/>
- <tile id="1" probability="0.11"/>
- <tile id="2" probability="0.11"/>
- <tile id="4" probability="0.05"/>
- <tile id="5" probability="0.11"/>
- <tile id="6" probability="0.11"/>
- <tile id="7" probability="0.11"/>
- <tile id="8" probability="0.11"/>
+<tileset version="1.9" tiledversion="1.9.2" name="fond_de_map" tilewidth="200" tileheight="200" tilecount="9" columns="3">
+ <image source="fond_de_map.png" width="600" height="600"/>
 </tileset>
